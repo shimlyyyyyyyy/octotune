f4edd15088ba7d3d56ab8ddb683448edd923b0f5
Rene Sternitzky <133014789+shimlyyyyyyyy@users.noreply.github.com>
2025-01-14 22:45:15 +0100

    Refactor user authentication in home.php; add error handling for welcome message and update script references

--- home.php
+++ home.php
@@ -7,39 +7,43 @@
   	function isUserLoggedIn(): bool{
 		$conn = new PDO("mysql:host=".servername."; dbname=".dbname, username, password);	
 		$uuid = $_COOKIE['uuid'];
-		
 		$sql = "SELECT * FROM benutzer where UUID = '$uuid'";
 		$result = $conn->query($sql);
-		
 		if (!(isset($_COOKIE['uuid']) && ($result->rowCount() > 0))){
-		unset($_COOKIE['uuid']);
-		header("Location: index.php");
+			unset($_COOKIE['uuid']);
+			header("Location: index.php");
 		}
-
 		return true;
   	}
 
 	function welcomeUser(): string{
-		$conn = new PDO("mysql:host=".servername."; dbname=".dbname, username, password);	
-		$uuid = $_COOKIE['uuid'];
-		$sql = "SELECT * FROM benutzer WHERE UUID = '$uuid'";
-		$result = $conn->query($sql);
-		$row = $result->fetch();
-		return "<h3 class='welcomeuser'><nobr>Welcome " . $row['username'] . "!</nobr></h3>";    
+		try{
+			$conn = new PDO("mysql:host=".servername."; dbname=".dbname, username, password);	
+			$uuid = $_COOKIE['uuid'];
+			$sql = "SELECT * FROM benutzer WHERE UUID = '$uuid'";
+			$result = $conn->query($sql);
+			$row = $result->fetch();
+			return "<h3 class='welcomeuser'><nobr>Welcome " . $row['username'] . "!</nobr></h3>";    
+		}
+		catch (Exception $e){
+			return "<h3 class='welcomeuser'><nobr>Welcome User!</nobr></h3>";
+		}
+		
 	}
+
+
   
-  if (isUserLoggedIn()){
-	echo '
+  if (!isUserLoggedIn()) die("index.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
  	<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OctoTune</title>
-    <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-    <link rel="stylesheet" href="/src/css/style.css">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>OctoTune</title>
+		<script src="/octotuneapp/src/App.js"></script>
+		<link rel="stylesheet" href="/src/css/style.css">
   	</head>
   	<body class="appbody">
       	<!-- topbar -->
@@ -57,7 +61,7 @@
         </form>
       	</div>
 		  	<div class="user">
-				'.welcomeUser().'
+				<?php echo welcomeUser(); ?>
 			</div>
         </div>
     </div>
@@ -65,17 +69,9 @@
     <div id="mydiv">
       
     </div>
-    	<script type="text/babel">
-			function Hello() {
-				return <h1>Hello World!</h1>;
-			}
-			const container = document.getElementById("mydiv");
-      		const root = ReactDOM.createRoot(container);
-      		root.render(<Hello />)
-    	</script>
+    	<script type="text/javascript" src="/octotuneapp/src/App.js">
+			App();
+
+		</script>
   	</body>
-</html>';
-}
-?>
-
-
+</html>'
